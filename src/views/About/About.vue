42798a8e2efdbd78abfa39734106f6b32619eba5
Levi Andrew <levi.andrew98@gmail.com>
2024-01-24 15:17:03 -0300

    Correção de texto na tela About

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -7,7 +7,7 @@
       <div class="max-content">
         <h1>trajetória_</h1>
         <p>
-          Olá! Me chamo Levi, nasci em Fortaleza-CE e atualmente tenho 25 anos. Sou apaixonado por esportes como futebol e futevôlei, além de ser adepto de uma vida saudável. Me em 2022 com minha conterrânea e alma gêmea, Thamires. A vida nos levou a Florianópolis-SC, onde concluí minha graduação em <b>Engenharia de Controle e Automação na UFSC</b>, formando-me em 12 de julho de 2023.
+          Olá! Me chamo Levi, nasci em Fortaleza-CE e atualmente tenho 25 anos. Sou apaixonado por esportes como futebol e futevôlei, além de ser adepto de uma vida saudável. Me casei em 2022 com minha conterrânea e alma gêmea, Thamires. A vida nos levou a Florianópolis-SC, onde concluí minha graduação em <b>Engenharia de Controle e Automação na UFSC</b>, formando-me em 12 de julho de 2023.
         </p>
         <p>
           Durante minha jornada acadêmica, enfrentei diversos desafios e adquiri experiência por meio de estágios em locais distintos. No <a href="https://eapn.ufsc.br/" target="_blank">Escritório de Automação de Processos</a> (EAP) no CTC-UFSC, desenvolvi habilidades com modelagem e documentação de processos. Posteriormente, no <a href="http://labtic.udesc.br/" target="_blank">LabTic</a> na ESAG-UDESC, desenvolvi minhas habilidades como <b>desenvolvedor web full-stack</b>, focando em Vue.js, Node.js, MongoDB, REST e socket.io, além de uma metodologia orietada a testes (TDD) utilizando Mocha e Chai. A experiência no <a href="http://labtic.udesc.br/" target="_blank">LabTic</a> foi tão enriquecedora que, além dos dois anos de estágio, continuo desenvolvendo sistemas web para a ESAG-UDESC por meio da minha MEI.
