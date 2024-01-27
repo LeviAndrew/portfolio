14b402ba8e1ea74576cfad53fe0696bcb966585e
Levi Andrew <levi.andrew98@gmail.com>
2024-01-27 12:17:47 -0300

    Atualizando data e referência

--- src/components/ViewMain/Footer/Footer.vue
+++ src/components/ViewMain/Footer/Footer.vue
@@ -3,7 +3,7 @@
     <div class="footer-content">
       <img src="../../../assets/images/logo-header.png" />
       <span>
-        Esse portfólio foi desenvolvido por Levi Andrew,
+        Esse portfólio foi desenvolvido por Levi Andrew, usando como referência o <a  href="https://github.com/guiialexandrino/"  target="_blank">github do gui</a>,
         utilizando:
       </span>
       <br />
@@ -14,7 +14,7 @@
           :style="`background-image: url(${getImage(icon)})`"
         ></div>
       </div>
-      <span>Última atualização: 23/01/2024.</span>
+      <span>Última atualização: 27/01/2024.</span>
     </div>
   </footer>
 </template>
